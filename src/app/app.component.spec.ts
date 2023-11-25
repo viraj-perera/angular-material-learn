d362b862d2c4772a627ba07ccdf7dabede386bce
viraj-perera <virajperera.k@gmail.com>
2023-11-25 06:39:47 +0530

    Added missing config files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ang-mat-learn'`, () => {
+  it(`should have as title 'angular-material-learn'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ang-mat-learn');
+    expect(app.title).toEqual('angular-material-learn');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ang-mat-learn');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-material-learn');
   });
 });
